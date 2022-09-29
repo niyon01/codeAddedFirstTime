dab0d858297b3bfabe701efb326d38d4426cc655
niyon01 <mdniyon2000@gmail.com>
2022-09-29 10:59:14 +0600

    add fibonacci series

---
+++ Fibonacci.java
@@ -0,0 +1,41 @@
+package com.learn_java;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.print("Enter Your Number : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        fibonacci(number);
+
+    }
+
+    private static void fibonacci(int number) {
+        int p = 0;
+        int i = 1;
+
+        if(number == 0){
+            System.out.println(p);
+            return ;
+        }else if(number == 1){
+            System.out.println(p);
+            System.out.println(i);
+            return ;
+        }else{
+            System.out.println(p);
+            System.out.println(i);
+        }
+
+        int count =2;
+        while (count<=number) {
+            int temp = i;
+            i = i + p;
+            p = temp;
+            System.out.println(i);
+            count = count + 1;
+
+        }
+
+    }
+}
