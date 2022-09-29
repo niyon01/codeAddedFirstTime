9ab12bb3a0d0edd2bf1d86f16c40c1974199e3e6
niyon01 <mdniyon2000@gmail.com>
2022-09-29 21:41:07 +0600

    String palindrome

--- Practice.java
+++ Practice.java
@@ -2,7 +2,23 @@ package com.learn_java;
 
 public class Practice {
     public static void main(String[] args) {
-       
 
+        String str = "dabcdcbad";
+        int left = 0;
+        int right = str.length() - 1;
+        boolean isPanidrome = true;
+        while (left <= right) {
+            if (str.charAt(left) == str.charAt(right)) {
+                left++;
+                right--;
+                continue;
+            } else {
+                isPanidrome= false;
+                break;
+            }
+        }
+        if (isPanidrome) {
+            System.out.println("yes");
+        } else System.out.println("no");
     }
 }
